c36f5cf6e8bbcd3f91685e5e47cce18b976f98f8
Erick <166533908+Erick01081@users.noreply.github.com>
2025-03-31 16:52:31 -0500

    Update StatusController.java

--- src/main/java/eci/ieti/safezone/controller/StatusController.java
+++ src/main/java/eci/ieti/safezone/controller/StatusController.java
@@ -8,6 +8,6 @@ public class StatusController {
 
     @GetMapping("/status")
     public String getStatus() {
-        return "The app is running and functional :) Yeiii Tonny Tonny Chopper";
+        return "The app is running and functional :)";
     }
 }
